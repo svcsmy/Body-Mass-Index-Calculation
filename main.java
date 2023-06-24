e38cae2eb9b53a4a1297a6c41c82dca43c79b87e
Sümeyye <58435258+svcsmy@users.noreply.github.com>
2023-06-24 20:51:27 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class main {
+
+ public static void main(String[] args) {
+	
+	// body-mass index calculation
+	 
+	 Scanner scanner = new Scanner(System.in);
+	 
+System.out.println("Please enter your height (1.64) ");	 
+double height = scanner.nextDouble();
+
+System.out.println("Please enter your weight(54)");
+int weight = scanner.nextInt();
+
+int index = (int)(weight * (height*height));
+
+System.out.println("Your body-mass index is: " + index);
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	
+ }
+}
